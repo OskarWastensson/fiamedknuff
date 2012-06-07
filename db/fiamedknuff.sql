1eeefbc8424bd4fa8e9508ae5663380de00b4e19
Oskar Wastensson <oskar@wastensson.se>
2012-06-07 15:48:59 +0200

    db

---
+++ db/fiamedknuff.sql
@@ -0,0 +1,83 @@
+-- phpMyAdmin SQL Dump
+-- version 3.4.10.1
+-- http://www.phpmyadmin.net
+--
+-- Värd: localhost
+-- Skapad: 01 jun 2012 kl 12:46
+-- Serverversion: 5.5.20
+-- PHP-version: 5.3.10
+
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Databas: `fiamedknuff`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Tabellstruktur `games`
+--
+
+CREATE TABLE IF NOT EXISTS `games` (
+  `id` int(20) unsigned NOT NULL AUTO_INCREMENT,
+  `status` varchar(50) COLLATE utf8_bin NOT NULL,
+  `date_created` datetime NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Tabellstruktur `moves`
+--
+
+CREATE TABLE IF NOT EXISTS `moves` (
+  `id` int(20) unsigned NOT NULL AUTO_INCREMENT,
+  `player_id` int(20) NOT NULL,
+  `date_moved` datetime NOT NULL,
+  `steps` int(2) NOT NULL,
+  `piece` int(2) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_bin AUTO_INCREMENT=2 ;
+
+-- --------------------------------------------------------
+
+--
+-- Tabellstruktur `players`
+--
+
+CREATE TABLE IF NOT EXISTS `players` (
+  `id` int(20) unsigned NOT NULL AUTO_INCREMENT,
+  `color` varchar(50) COLLATE utf8_bin NOT NULL,
+  `game_id` int(20) NOT NULL,
+  `user_id` int(20) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `user_id` (`user_id`),
+  KEY `game_id` (`game_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Tabellstruktur `users`
+--
+
+CREATE TABLE IF NOT EXISTS `users` (
+  `id` int(20) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(100) COLLATE utf8_bin NOT NULL,
+  `email` varchar(100) COLLATE utf8_bin NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `email` (`email`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin AUTO_INCREMENT=1 ;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
