62c999c6823cb4fdec03ee0c6d405599c6bfdfe9
Oskar Wastensson <oskar@wastensson.se>
2012-06-14 13:43:47 +0200

    Piece object and canMove function

---
+++ scripts/piece.js
@@ -0,0 +1,65 @@
+// This file conatins all logic relating to a peice
+
+var Piece = function (brick, player) {
+	var canMove, isCrash, canSprint, isAtGoal, 
+		move, Crash, startSprint, makeGoal,
+		currentBrick = brick;
+	
+	canMove = function (steps) {
+		var brick, exit = false;
+		// Test if the piece is at home
+		if (currentBrick.name === 'home' + player.colorName) {
+			if (steps === 1 || steps === 6) {
+				return true;
+			} else {
+				return false;
+			}
+		};
+
+		// Test if any of the bricks on the path contains 
+		// a peice of the same player
+		brick = currentBrick;
+		for (var i = 1; i < steps; i += 1) {
+			brick = brick.nextBrick; // OR something..
+			$.foreach(brick.pieces, function (piece) {
+				if (piece.player == player) {
+					exit = true;
+				};
+			});
+			if(exit) {
+				return false;
+			}
+		}
+		return true;
+	};
+	
+	isCrash = function () {
+		
+	};
+	
+	canSprint = function () {
+		
+	};
+	
+	isAtGoal = function () {
+		
+	};
+	
+	move = function (steps) {
+		
+	};
+	
+	crash = function () {
+		
+	};
+	
+	sprint = function () {
+		
+	};
+	
+	makeGoal = function () {
+		
+	};
+	
+} 
+
