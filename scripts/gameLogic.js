a087cb0e2d557a5fa7e35dc09c8c1355cee3013f
Hector Romo <hectitor.romo@gmail.com>
2012-06-19 07:36:53 +0200

    game logic update
    Gets previous and next brick now. Finds current brick that has class
    'current'.

--- scripts/gameLogic.js
+++ scripts/gameLogic.js
@@ -2,9 +2,9 @@ var player, bricks;
 
 $(document).ready(function() {
 	
+	bricks = [];
 
-	var bricks = [];
-
+	// Puts every game-brick ordered in array bricks.
 	$(".boardWrapper span.outer-bricks").each(function() {
 		var span = $(this);
 		var val = $(this).attr("value");
@@ -13,37 +13,48 @@ $(document).ready(function() {
 
 	})
 
+	// Defines class Game, here you can get previous and next brick.
 
-	// Varje bricka måste veta vilka grannar den har.
+	function Game(currentValue) {
+		
+		this.currentValue = currentValue;
 
-	var current 		= bricks[0],
-		currentValue 	= current.attr('value'),
-		nextValue		= parseInt(currentValue)+1;
+		// Gets element "next brick"
+		// if current brick is 39, next brick starts from 0.
+		this.nextBrick = function() {
+			var nextValue = currentValue
+			
+			if( currentValue == 39 ){
+				nextValue = 0;
+			}else{
+				nextValue = parseInt(currentValue)+1;
+			}
+			return bricks[nextValue];
+		};
 
-
-		console.log(nextValue);
-
-	
-	//Vi måste vilken som är current brick
-	//Varje bricka måste veta vilka grannar den har. 
-	//När man kommer in på sprint så ändras reglerna.
-
-
-	function GameBoard() {
-		this.next = current+1;
+		// Gets element "previous brick"
+		// if current brick is 0, previous brick is from 39.
+		this.prevBrick = function() {
+			var prevValue = currentValue
+			
+			if( currentValue == 0 ){
+				prevValue = 39;
+			}else{
+				prevValue = parseInt(currentValue)-1;
+			}
+			return bricks[prevValue];
+		}
 	}
 
 
+	var currentBrick = $('.boardWrapper').find('.current');
+		currentValue = currentBrick.attr('value');
 
-	$('.boardWrapper span').each(function() {
-		
-		if( $(this).has('value') ){
-			
-			bricks.push($(this));	
 
-		}
+	var game = new Game(currentBrick, currentValue);
 
-	});
+
+
 
 
 })
